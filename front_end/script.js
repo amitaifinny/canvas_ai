ee1ff7b3729c17c748f192387fdb0bc3f328bbc4
Mckayfinny <mckayfinny@gmail.com>
2025-05-28 23:43:15 -0600

    started coding the javascript. Added hooks.

---
+++ front_end/script.js
@@ -0,0 +1,7 @@
+const chatContainer = document.getElementById('chat-container');
+const openChatButton = document.getElementById('ai-button');
+const closeChatButton = document.getElementById('back-button');
+const messageInput = document.getElementById('message-input');
+const sendButton = document.getElementById('send-button');
+const messageArea = document.getElementById('chat-input-area');
+const ShowClass = document.getElementById('show-classes');
